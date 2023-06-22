81775e62a173b77c6d01d6fb02ce0e20c128b2d9
“davegd1387” <davegd1387@gmailcom>
2023-06-22 10:20:01 -0400

    Adjusted API URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://the-widget-folks-serverless.netlify.app/",
+  baseURL: "https://the-widget-folks-serverless.netlify.app",
   // baseURL: "http://localhost:4000",
 });
